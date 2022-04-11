4f836db578bb1ffc2fc41b1e9e0b432c7d680702
ConstantinJun <94495420+ConstantinJun@users.noreply.github.com>
2022-04-11 21:36:46 +0300

    create project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './app',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+};
